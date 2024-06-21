2479c89335c77d5921c3b14dd8101248aaf71b67
kamigear <richieleonardo20@gmail.com>
2024-06-21 12:38:41 +0700

    better orientataion and structure

--- javascript.js
+++ javascript.js
@@ -14,64 +14,53 @@ let lastoperation = "";
 let havedot = "";
 
 numberel.foreach((number) => {
-	number.addEventListener("click", (e) => 
-	{
-		if (e.target.innerText === "." && !havedot)
-		{
-			havedot = true;
-		}
-		else if (e.target.innerText === "." && havedot) {
-			return = true;
-		}
-		dis2num += e.target.innerText;
-		displaye2el.innerText = dis2num;
-
-
-	});
+    number.addEventListener("click", (e) => {
+        if (e.target.innerText === "." && !havedot) {
+            havedot = true;
+        } else if (e.target.innerText === "." && havedot) {
+            return = true;
+        }
+        dis2num += e.target.innerText;
+        displaye2el.innerText = dis2num;
+    });
 });
 
 operationel.foreach((operation) => {
-	operation.addEventListener("click", (e) =>
-	{
-		if (!dis2num) {
-			return;
-		}
-		havedot = false;
-		const operationname = e.target.innerText;
-		if (dis1num && dis2num && lastoperation) {
-			mathoperation(); 
-		}
-		else{
-			result = parseFloat(dis2num);
-		}
-		clearVar(operationname);
-		lastoperation = operationname;
-		console.log(result);
-	});
+    operation.addEventListener("click", (e) => {
+        if (!dis2num) {
+            return;
+        }
+        havedot = false;
+        const operationname = e.target.innerText;
+        if (dis1num && dis2num && lastoperation) {
+            mathoperation();
+        } else {
+            result = parseFloat(dis2num);
+        }
+        clearVar(operationname);
+        lastoperation = operationname;
+        console.log(result);
+    });
 });
 
 function clearVar(name = " ") {
-	dis1num += dis2num + " " + name + " ";
-	displaye1.innerText = dis1num;
-	displaye2.innerText = "";
-	dis2num = "";
-	tempresult.innerText = result;
+    dis1num += dis2num + " " + name + " ";
+    displaye1.innerText = dis1num;
+    displaye2.innerText = "";
+    dis2num = "";
+    tempresult.innerText = result;
 }
 
-function mathoperation(){
-	if (lastoperation === "x") {
-		result = parseFloat(result) * parseFloat(dis2num);
-	}
-	else if(lastoperation === "+") {
-		result = parseFloat(result) + parseFloat(dis2num);
-	}
-	else if(lastoperation === "-") {
-		result = parseFloat(result) - parseFloat(dis2num);
-	}
-	else if(lastoperation === "/") {
-		result = parseFloat(result) / parseFloat(dis2num);
-	}
-	else if(lastoperation === "%") {
-		result = parseFloat(result) % parseFloat(dis2num);
-	}
+function mathoperation() {
+    if (lastoperation === "x") {
+        result = parseFloat(result) * parseFloat(dis2num);
+    } else if (lastoperation === "+") {
+        result = parseFloat(result) + parseFloat(dis2num);
+    } else if (lastoperation === "-") {
+        result = parseFloat(result) - parseFloat(dis2num);
+    } else if (lastoperation === "/") {
+        result = parseFloat(result) / parseFloat(dis2num);
+    } else if (lastoperation === "%") {
+        result = parseFloat(result) % parseFloat(dis2num);
+    }
 }
